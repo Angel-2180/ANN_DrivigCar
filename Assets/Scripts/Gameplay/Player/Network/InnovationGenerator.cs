e530d45f4ea2d21107c40feb6c78eea740fabd15
Angel-2180 <a.gimer@student.isartdigital.com>
2024-03-15 02:17:12 -0400

    Refactor InnovationGenerator class to simplify GetInnovation method

--- Assets/Scripts/Gameplay/Player/Network/InnovationGenerator.cs
+++ Assets/Scripts/Gameplay/Player/Network/InnovationGenerator.cs
@@ -1,26 +1,14 @@
 public class InnovationGenerator
 {
     private int innovation;
-    private int[] innovationNumbers;
-    private int innovationIndex;
 
     public InnovationGenerator()
     {
         innovation = 0;
-        innovationNumbers = new int[1000];
-        innovationIndex = 0;
     }
 
     public int GetInnovation()
     {
-        innovation++;
-        innovationNumbers[innovationIndex] = innovation;
-        innovationIndex++;
-        return innovation;
-    }
-
-    public int GetInnovation(int index)
-    {
-        return innovationNumbers[index];
+        return innovation++;
     }
 }
