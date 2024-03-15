9158e27a21760645b354d4b66f549193ab12fc08
Angel-2180 <a.gimer@student.isartdigital.com>
2024-03-15 02:17:29 -0400

    Add GenomePrinter class and debug genome in NetworkTrainer

---
+++ Assets/Scripts/Gameplay/Player/Network/Debug/GenomePrinter.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenomePrinter
+{
+    public static void PrintGenome(Genome genome, string path)
+    {
+        Debug.Log("Printing genome");
+        foreach (NodeGenes node in genome.GetNodeGenes().Values)
+        {
+            Debug.Log("Node: " + node.GetId() + " Type: " + node.GetType());
+        }
+        foreach (ConnectionGenes connection in genome.GetConnectionGenes().Values)
+        {
+            if (!connection.IsExpressed())
+            {
+                continue;
+            }
+            Debug.Log("Connection: " + connection.GetInNode() + " -> " + connection.GetOutNode() + " Weight: " + connection.GetWeight() + " Expressed: " + connection.IsExpressed() + " Innovation: " + connection.GetInnovationNumber());
+        }
+
+        //draw genome in ascii art
+        string genomeString = "";
+        genomeString += "Nodes:\n";
+        foreach (NodeGenes node in genome.GetNodeGenes().Values)
+        {
+            genomeString += "Node: " + node.GetId() + " Type: " + node.GetType() + "\n";
+        }
+        genomeString += "Connections:\n";
+        foreach (ConnectionGenes connection in genome.GetConnectionGenes().Values)
+        {
+            if (!connection.IsExpressed())
+            {
+                continue;
+            }
+            genomeString += "Connection: " + connection.GetInNode() + " -> " + connection.GetOutNode() + " Weight: " + connection.GetWeight() + " Expressed: " + connection.IsExpressed() + " Innovation: " + connection.GetInnovationNumber() + "\n";
+        }
+        Debug.Log(genomeString);
+    }
+}
--- Assets/Scripts/Gameplay/Player/Network/NetworkTrainer.cs
+++ Assets/Scripts/Gameplay/Player/Network/NetworkTrainer.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class NetworkTrainer : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void Start() //debug genome here
     {
-        
+        InnovationGenerator innovation = new InnovationGenerator();
+        Genome genome = new Genome();
+
+        NodeGenes input1 = new NodeGenes(NodeType.Input, 0);
+        NodeGenes input2 = new NodeGenes(NodeType.Input, 1);
+        NodeGenes output1 = new NodeGenes(NodeType.Output, 2);
+
+        ConnectionGenes connection1 = new ConnectionGenes(0, 2, 1f, true, innovation.GetInnovation());
+        ConnectionGenes connection2 = new ConnectionGenes(1, 2, 1f, true, innovation.GetInnovation());
+
+        genome.AddNodeGene(input1);
+        genome.AddNodeGene(input2);
+        genome.AddNodeGene(output1);
+
+        genome.AddConnectionGene(connection1);
+        genome.AddConnectionGene(connection2);
+
+        GenomePrinter.PrintGenome(genome, "Assets/Genome_pre_NodeMut.png");
+
+        genome.AddNodeMutation(innovation);
+
+        GenomePrinter.PrintGenome(genome, "Assets/Genome_post_NodeMut.png");
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
     }
 }
