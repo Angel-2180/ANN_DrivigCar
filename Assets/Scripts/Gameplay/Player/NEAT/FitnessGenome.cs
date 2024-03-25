952f54285fa1ac64266a9cc05f9832e9a3a25070
Angel-2180 <a.gimer@student.isartdigital.com>
2024-03-25 11:39:02 -0400

    Add FitnessGenome class

---
+++ Assets/Scripts/Gameplay/Player/NEAT/FitnessGenome.cs
@@ -0,0 +1,11 @@
+public class FitnessGenome
+{
+    public Genome genome;
+    public float fitness;
+
+    public FitnessGenome(Genome genome, float fitness)
+    {
+        this.genome = genome;
+        this.fitness = fitness;
+    }
+}
