401fac974230457e7f96bbccb7be12347f1dde2c
Angel-2180 <a.gimer@student.isartdigital.com>
2024-03-26 03:07:11 -0400

    Refactor GenesisGenomeProvider class

--- Assets/Scripts/Gameplay/Player/NEAT/GenesisGenomeProvider.cs
+++ Assets/Scripts/Gameplay/Player/NEAT/GenesisGenomeProvider.cs
@@ -1,4 +1,16 @@
-﻿public interface IGenesisGenomeProvider
+﻿using System;
+
+public class GenesisGenomeProvider
 {
-    public Genome GenerateGenesisGenome();
+    public GenesisGenomeProvider()
+    {
+        GenerateGenesisGenome = GenerateGenesisGenomeImpl;
+    }
+
+    public Func<Genome> GenerateGenesisGenome;
+
+    public virtual Genome GenerateGenesisGenomeImpl()
+    {
+        throw new NotImplementedException();
+    }
 }
